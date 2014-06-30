37f857ab7036200c09fadcb4fb0466d480e3e7e8
Brian Link <cpsubrian@gmail.com>
2014-06-30 15:57:57 -0700

    Updates to newer js-yaml.

--- etc-yaml.js
+++ etc-yaml.js
@@ -1,10 +1,11 @@
-require('js-yaml');
+var yaml = require('js-yaml')
+  , fs = require('fs');
 
 exports.attach = function() {
   var etc = this;
 
   etc.parseYAML = function(filePath) {
-    return require(filePath);
+    return yaml.safeLoad(fs.readFileSync(filePath, 'utf8'));
   };
 
   etc.parsers.yaml = etc.parseYAML;
